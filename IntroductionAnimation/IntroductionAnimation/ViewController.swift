1e46e0bb8bcb03ccd2c8afcecdce5b5944a7a26e
songjucho <pinetree134@ajou.ac.kr>
2021-03-03 18:45:03 +0900

    feat: 타이머 구현

--- IntroductionAnimation/IntroductionAnimation/ViewController.swift
+++ IntroductionAnimation/IntroductionAnimation/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
     private let letters = ["@", "%", "*", "^", "1", "2", "3", " "]
     private var wordsForDisplaying = ["SONG", "JU", "'S", "GIT", "HUB", ""]
     private var randomLetters = ""
+    private var incrementer = 0
+    private var timer: Timer?
+    private var internalTimer: Timer?
+    private var mainTimer: Timer?
+    
+    @IBOutlet weak var animatingLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +24,35 @@ class ViewController: UIViewController {
     }
     
     private func animate() {
-        
+        mainTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            self.internalTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+                for _ in 0...arc4random_uniform(UInt32(10) + 1) {
+                    let randomNumber = arc4random_uniform(UInt32(self.letters.count - 1))
+                    self.randomLetters += self.letters[Int(randomNumber)]
+                }
+                self.animatingLabel.text = self.randomLetters
+                self.animatingLabel.font = self.animatingLabel.font.withSize(20)
+                self.animatingLabel.alpha = 0.5
+                self.randomLetters = ""
+            })
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.7, repeats: false, block: { _ in
+                if self.incrementer != self.wordsForDisplaying.count - 1 {
+                    self.internalTimer?.invalidate()
+                    self.animatingLabel.text = self.wordsForDisplaying[self.incrementer]
+                    self.animatingLabel.font = self.animatingLabel.font.withSize(28)
+                    self.animatingLabel.alpha = 1
+                    self.incrementer += 1
+                    
+                } else {
+                    self.timer?.invalidate()
+                    self.internalTimer?.invalidate()
+                    self.mainTimer?.invalidate()
+                    self.animatingLabel.font = self.animatingLabel.font.withSize(28)
+                    self.animatingLabel.alpha = 1
+                    self.animatingLabel.text = "❤️"
+                }
+            })
+        })
     }
-
-
 }
 
