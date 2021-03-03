90cfff17526b0821d83d546703b2699d18aaf3bc
songjucho <pinetree134@ajou.ac.kr>
2021-03-03 18:25:57 +0900

    feat: 프로젝트 생성

---
+++ IntroductionAnimation/IntroductionAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroductionAnimation
+//
+//  Created by 송주 on 2021/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
