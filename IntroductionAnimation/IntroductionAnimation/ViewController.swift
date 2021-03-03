3dc3c667d1811218de4067e0d8fc822a201cec60
songjucho <pinetree134@ajou.ac.kr>
2021-03-03 18:29:59 +0900

    feat: 단어들이 담긴 리스트 변수 생성

--- IntroductionAnimation/IntroductionAnimation/ViewController.swift
+++ IntroductionAnimation/IntroductionAnimation/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let letters = ["@", "%", "*", "^", "1", "2", "3", " "]
+    private var wordsForDisplaying = ["SONG", "JU", "'S", "GIT", "HUB", ""]
+    private var randomLetters = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
